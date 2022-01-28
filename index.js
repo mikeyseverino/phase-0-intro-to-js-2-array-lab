932495089b730f4dc686e96d3de1b270872e283e
Michael Severino <mikeyseverino@gmail.com>
2022-01-27 21:02:50 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+destructivelyPrependCat("Bob");
+
+function destructivelyRemoveLastCat(name){
+    return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name);
+}
+
+function appendCat(name){
+    return [...cats, name];
+}
+appendCat("Broom");
+
+function prependCat(name){
+    return [name, ...cats];
+}
+prependCat("Arnold");
+
+function removeLastCat(name){
+    return cats.slice(0,-1);
+}
+removeLastCat();
+
+function removeFirstCat(name){
+    return cats.slice(1);
+}
+removeFirstCat;
